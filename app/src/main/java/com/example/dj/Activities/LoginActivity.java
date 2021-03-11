f8984d899fe1c93973a66fe6b327d3a39888f5f7
hadas <hadas.khri@gmail.com>
2021-03-11 13:04:33 +0200

    Added capabilities: 1. song request per DJ, and display in custom listView-->writing to "playlist" node in realtime db   1.1 created SongAdapter.java   1.2 created requested_song_list_item.xml   1.3 added requestSong func in Fragment_song request   1.4 implement DjsRequestedSongsFragment.java
    2. creating feedback per DJ, and display in custom listview-->writing to "djPlaylist" node in realtime db
        2.1 created Feedback.java
        2.2 created FeedbackAdapter
        2.3 created custom feedback_list_item.xml
        2.4 added sendFeedback function  func in FeedbackFragment.java
        2.5 implement DjsFeedbacksListFragment.java

--- app/src/main/java/com/example/dj/Activities/LoginActivity.java
+++ app/src/main/java/com/example/dj/Activities/LoginActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -23,11 +24,19 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.regex.Pattern;
+
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;//firebase login with email and password
     SharedPreferences sharedPreferences;
     private final String KEY1 = "LoginKeyName";
+    private static final Pattern PASSWORD_PATTERN =
+            Pattern.compile("^" +
+                    "(?=.*[0-9])" +  // at list 1 digit
+                    "(?=\\S+$)" +  // no white spaces
+                    ".{6,}" + // at list 6 characters minimum
+                    "$");
 
 
     @Override
@@ -48,70 +57,91 @@ public class LoginActivity extends AppCompatActivity {
         //casting the text view to string
         String email = t.getText().toString();
         //finding the email Editiew to t
+        //Email Validate
+        if (email.isEmpty()) {
+            t.setError("Email can't be empty");
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            t.setError("Please enter a valid email address");
+        }
+
         t = findViewById(R.id.password1);
         //casting the text view to string
         String password = t.getText().toString();
 
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Toast.makeText(LoginActivity.this, "Login Successfull.",
-                                    Toast.LENGTH_LONG).show();
-
-                            //save the email and password to to cookie (shared preferences)
-                            sharedPreferences = getPreferences(MODE_PRIVATE);
-
-                            SharedPreferences.Editor editor = sharedPreferences.edit();
-                            editor.putString("keyUser", email);
-                            editor.putString("keyPass", password);
-                            editor.apply();
 
 
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            String uid = user.getUid();
-                            DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("users").child(uid);
 
-                            // Read User Object from the database
-                            myRef.addValueEventListener(new ValueEventListener() {
-                                @Override
-                                public void onDataChange(DataSnapshot dataSnapshot) {
-                                    // This method is called once with the initial value and again
-                                    // whenever data at this location is updated.
-                                    User value = dataSnapshot.getValue(User.class);
-                                    if(value.getUserType()=="Clubber"){
-                                        funcButtonToClubber1Activity();
+        //PassWord Validate
+        if (password.isEmpty()) {
+            t.setError("Password can't be empty");
+        } else if (!PASSWORD_PATTERN.matcher(password).matches()) {
+            t.setError("Password is not valid");
+        }
 
+
+        if (!email.isEmpty() && !password.isEmpty()) {
+
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Toast.makeText(LoginActivity.this, "Login Successfull.",
+                                        Toast.LENGTH_LONG).show();
+
+                                //save the email and password to to cookie (shared preferences)
+                                sharedPreferences = getPreferences(MODE_PRIVATE);
+
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putString("keyUser", email);
+                                editor.putString("keyPass", password);
+                                editor.apply();
+
+
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                String uid = user.getUid();
+                                DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("users").child(uid);
+
+                                // Read User Object from the database
+                                myRef.addValueEventListener(new ValueEventListener() {
+                                    @Override
+                                    public void onDataChange(DataSnapshot dataSnapshot) {
+                                        // This method is called once with the initial value and again
+                                        // whenever data at this location is updated.
+                                        User value = dataSnapshot.getValue(User.class);
+                                        if (value.getUserType() == "Clubber") {
+                                            funcButtonToClubber1Activity();
+
+                                        } else {
+                                            funcButtonToDjActivity(uid);
+                                        }
                                     }
-                                    else{
-                                        funcButtonToDjActivity(uid);
+
+                                    @Override
+                                    public void onCancelled(DatabaseError error) {
+                                        // Failed to read value
+                                        Toast.makeText(LoginActivity.this, "DB reading  failed.",
+                                                Toast.LENGTH_LONG).show();
                                     }
-                                }
-
-                                @Override
-                                public void onCancelled(DatabaseError error) {
-                                    // Failed to read value
-                                    Toast.makeText(LoginActivity.this, "DB reading  failed.",
-                                            Toast.LENGTH_LONG).show();
-                                }
-                            });
+                                });
 
 
-                        } else {
-                            // If sign in fails, display a message to the user.
+                            } else {
+                                // If sign in fails, display a message to the user.
 
-                            Toast.makeText(LoginActivity.this, "Email or Password is incoreect.",
-                                    Toast.LENGTH_LONG).show();
+                                Toast.makeText(LoginActivity.this, "Email or Password is incoreect.",
+                                        Toast.LENGTH_LONG).show();
 
+                            }
+
+                            // ...
                         }
-
-                        // ...
-                    }
-                });
+                    });
 
 
+        }
     }
 
     private void funcButtonToDjActivity(String uid) {
