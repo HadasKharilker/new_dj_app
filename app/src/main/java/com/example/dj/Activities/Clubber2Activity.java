c40086f24be1f046f9d02aadc6250bc042f0ab4d
hadas <hadas.khri@gmail.com>
2021-03-11 21:33:02 +0200

    fixed issues: 1: when connecting as clubber we moved to dj unstead of Clubber2Activity- fixed 2: when moving between buttons in Clubber2Activity the app crashed- fixed

--- app/src/main/java/com/example/dj/Activities/Clubber2Activity.java
+++ app/src/main/java/com/example/dj/Activities/Clubber2Activity.java
@@ -1,11 +1,14 @@
 package com.example.dj.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +23,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Clubber2Activity extends AppCompatActivity {
     private FragmentManager fragmentManager;
@@ -38,6 +44,7 @@ public class Clubber2Activity extends AppCompatActivity {
         nameOfDj = getIntent().getStringExtra("KEY2");//getting the name of the Dj , that the user choose in the djs list fragment
         textView = findViewById(R.id.nameOfDJ);
         textView.setText("DJ " + nameOfDj);
+        setDjRating();
         moveToFeedbackFragment();
 
         //fragmentManager = getSupportFragmentManager();// transit fragment to activity
@@ -47,23 +54,6 @@ public class Clubber2Activity extends AppCompatActivity {
 
 
     public void inputfeedback(View view) {
-        fragmentManager = getSupportFragmentManager();// transit fragment to activity
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_dj, new FeedbackFragment()).commit();
-    }
-
-    public void songrequest(View view) {
-        fragmentManager = getSupportFragmentManager();// transit fragment to activity
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_dj, new SongRequestFragment()).commit();
-    }
-
-    public String getNameOfDj(){
-
-        return nameOfDj;
-    }
-
-    public void moveToFeedbackFragment(){
         //get the user object
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
@@ -82,13 +72,64 @@ public class Clubber2Activity extends AppCompatActivity {
                 // whenever data at this location is updated.
                 fragmentManager = getSupportFragmentManager();
                 User value = dataSnapshot.getValue(User.class);
-                userName= value.getFullName();
+                userName = value.getFullName();
                 Bundle data = new Bundle();//Use bundle to pass data
                 data.putString("data", userName);//put string, int, etc in bundle with a key value
-                FeedbackFragment fragInfo= new FeedbackFragment();
+                FeedbackFragment fragInfo = new FeedbackFragment();
                 fragInfo.setArguments(data);
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                fragmentTransaction.add(R.id.fragment_dj,fragInfo).addToBackStack(null).commit();
+                fragmentTransaction.add(R.id.fragment_dj, fragInfo).commit();
+
+
+            }
+
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+
+            }
+        });
+
+    }
+
+    public void songrequest(View view) {
+        fragmentManager = getSupportFragmentManager();// transit fragment to activity
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_dj, new SongRequestFragment()).commit();
+    }
+
+    public String getNameOfDj() {
+
+        return nameOfDj;
+    }
+
+    public void moveToFeedbackFragment() {
+        //get the user object
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        //extract the user id
+        String uid = user.getUid();
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("users").child(uid);
+
+
+        // Read User Object from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                fragmentManager = getSupportFragmentManager();
+                User value = dataSnapshot.getValue(User.class);
+                userName = value.getFullName();
+                Bundle data = new Bundle();//Use bundle to pass data
+                data.putString("data", userName);//put string, int, etc in bundle with a key value
+                FeedbackFragment fragInfo = new FeedbackFragment();
+                fragInfo.setArguments(data);
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.add(R.id.fragment_dj, fragInfo).commit();
 
 
             }
@@ -102,10 +143,76 @@ public class Clubber2Activity extends AppCompatActivity {
         });
 
 
-
     }
 
+    public void setDjRating() {
+
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference songsRef = rootRef.child("users");//users branch reference from firebase database
+
+        ValueEventListener eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {//for each LOOP running through all songs
+
+                    String fullName = ds.child("fullName").getValue(String.class);
+                    if (fullName.equals(nameOfDj)) { //
+                        String djUID;
+                        djUID = ds.child("id").getValue(String.class); //extrecting dj id
 
 
+                        //geting all ratings of dj
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
 
+                        //going to the relevant branch in the db
+                        final DatabaseReference myRef = database.getReference("djFeedback");
+                        DatabaseReference feedbackRoot = myRef.child(djUID);
+                        ValueEventListener eventListener1 = (new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                                    int count = 0;
+                                    int totalStars = 0;
+                                    float djAverage = 0;
+                                    if(snapshot.hasChildren()) {
+
+
+                                        for (DataSnapshot ds : snapshot.getChildren()) {
+                                            Map<String, Object> mHashmap = new HashMap<>();
+                                            mHashmap = (Map<String, Object>) ds.getValue();
+                                            String rating = String.valueOf(mHashmap.get("rating"));
+
+                                            totalStars += Integer.valueOf(rating);
+                                            count += 1;
+
+                                        }
+                                        djAverage = totalStars / count;
+                                        RatingBar rating = (RatingBar) findViewById(R.id.rating);
+                                        rating.setRating(djAverage);
+                                    }
+
+
+                            }
+
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError error) {
+
+                            }
+                        });
+                        feedbackRoot.addListenerForSingleValueEvent(eventListener1);
+
+
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        };
+        songsRef.addListenerForSingleValueEvent(eventListener);
+
+
+    }
 }
